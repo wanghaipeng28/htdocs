dde3665f38eb904d102214a54506546642d7dcfd
wanghaipeng28 <wanghaipeng28@163.com>
2016-11-23 13:21:00 +0800

    updata statistics.js1123

--- table-demo/js/statistics.js
+++ table-demo/js/statistics.js
@@ -7,21 +7,54 @@
     }catch (err){
         throw new Error("依赖的jQuery函数不存在！");
     }
-    var aD=[];
+    var groupData=null;
     function statistics(data,arr){
         if(arr.length){
-            a(data,arr);
+            return a(data,arr);
         }else{
             return data;
         }
     }
     function a(d,arr){
+        var aD={};
         $.each(d,function(i,v){
-
-            //$.each(val,function(item,val){
-            //
-            //});
+            groupData=aD;
+            $.each(arr,function(iArr,vArr){
+                var groupName=v[vArr];
+                if(groupData[groupName]){
+                    $.each(v,function(item,val){
+                        if(typeof(val)=="number"){
+                            groupData[groupName][item]+=val;
+                        }
+                    });
+                }else{
+                    groupData[groupName]={};
+                    $.each(v,function(item,val){
+                        if(typeof(val)=="string"){
+                            if(item=="groupName"){
+                                groupData[groupName][item]= groupName;
+                            }else{
+                                groupData[groupName][item]="";
+                            }
+                        }else if(typeof(val)=="number"){
+                            if(!groupData[groupName][item]){
+                                groupData[groupName][item]=0;
+                            }
+                            groupData[groupName][item]+=val;
+                        }
+                    });
+                    if(iArr==arr.length-1){
+                        groupData[groupName].data=[];
+                    }else{
+                        groupData[groupName].data={};
+                    }
+                    groupData[groupName].level=iArr+1;
+                }
+                groupData=groupData[groupName].data;
+            });
+            groupData.push(v);
         });
+        return aD;
     }
     $.statistics=statistics;
 })()
